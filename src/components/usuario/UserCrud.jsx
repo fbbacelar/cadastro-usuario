5fae8d9f2500ab7914a646f8d6be6ac528730853
Fábio Bacelar <fabibbacelar@gmail.com>
2020-11-06 22:09:30 -0300

    Pequenas correções de advertências

--- src/components/usuario/UserCrud.jsx
+++ src/components/usuario/UserCrud.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Main from '../template/Main'
-import Userform from './Userform'
-import UserTable from './UserTable'
-import axios from 'axios'
+import React from 'react';
+import Main from '../template/Main';
+import Userform from './Userform';
+import UserTable from './UserTable';
+import axios from 'axios';
 
-import { NotificationContainer, NotificationManager } from 'react-notifications'
+import { /*NotificationContainer,*/ NotificationManager } from 'react-notifications';
 
 const headerProps = {
     icon: 'users',
     title: 'Usuários',
     subtitle: 'Cadastro de Usuários: Incluir, Listar, Alterar e Excluir'
-}
+};
 
-const baseUrl = 'http://localhost:8080/usuarios'
+const baseUrl = 'http://localhost:8080/usuarios';
 
 const initialState = {
     usuario: {
@@ -22,85 +22,85 @@ const initialState = {
         senha: ''
     },
     list: []
-}
+};
 
 export default class UserCrud extends React.Component {
 
     constructor() {
-        super()
+        super();
 
-        this.state = { ...initialState }
+        this.state = { ...initialState };
 
-        this.clear = this.clear.bind(this)
-        this.save = this.save.bind(this)
-        this.getUpdatedList = this.getUpdatedList.bind(this)
-        this.updateField = this.updateField.bind(this)
-        this.load = this.load.bind(this)
-        this.remove = this.remove.bind(this)
-        this.handleEnterPress = this.handleEnterPress.bind(this)
+        this.clear = this.clear.bind(this);
+        this.save = this.save.bind(this);
+        this.getUpdatedList = this.getUpdatedList.bind(this);
+        this.updateField = this.updateField.bind(this);
+        this.load = this.load.bind(this);
+        this.remove = this.remove.bind(this);
+        this.handleEnterPress = this.handleEnterPress.bind(this);
     }
 
     componentWillMount() {
         axios.get(baseUrl).then(response => {
-            this.setState({ list: response.data })
-        })
+            this.setState({ list: response.data });
+        });
     }
 
     handleEnterPress(event) {
         if (event.key === 'Enter') {
-            this.save()
+            this.save();
         }
     }
 
     clear() {
-        this.setState({ usuario: initialState.usuario })
+        this.setState({ usuario: initialState.usuario });
     }
 
     save() {
-        const usuario = this.state.usuario
-        const method = usuario.id ? 'put' : 'post'
-        const url = usuario.id ? `${baseUrl}/${usuario.id}` : baseUrl
+        const usuario = this.state.usuario;
+        const method = usuario.id ? 'put' : 'post';
+        const url = usuario.id ? `${baseUrl}/${usuario.id}` : baseUrl;
 
         if (usuario.nome === '' || usuario.email === '' || /*usuario.telefone === '' ||*/ usuario.senha === '') {
-            NotificationManager.warning('Nome, e-mail e senha obrigatórios', 'Preencha os campos')
+            NotificationManager.warning('Nome, e-mail e senha obrigatórios', 'Preencha os campos');
         } else {
             axios[method](url, usuario)
                 .then(response => {
-                    const list = this.getUpdatedList(response.data)
-                    this.setState({ usuario: initialState.usuario, list })
+                    const list = this.getUpdatedList(response.data);
+                    this.setState({ usuario: initialState.usuario, list });
                     if (method === 'post')
-                        NotificationManager.success('Usuário criado com sucesso', 'Criar Usuário')
+                        NotificationManager.success('Usuário criado com sucesso', 'Criar Usuário');
                     else
-                        NotificationManager.success('Usuário alterado com sucesso', 'Editar Usuário')
-                })
+                        NotificationManager.success('Usuário alterado com sucesso', 'Editar Usuário');
+                });
         }
 
     }
 
     getUpdatedList(usuario, add = true) {
-        const list = this.state.list.filter(u => u.id !== usuario.id)
+        const list = this.state.list.filter(u => u.id !== usuario.id);
         if (add)
-            list.push(usuario)
-        return list
+            list.push(usuario);
+        return list;
     }
 
     updateField(event) {
-        const usuario = { ...this.state.usuario }
-        usuario[event.target.name] = event.target.value
-        this.setState({ usuario })
+        const usuario = { ...this.state.usuario };
+        usuario[event.target.name] = event.target.value;
+        this.setState({ usuario });
     }
 
     load(usuario) {
-        this.setState({ usuario })
+        this.setState({ usuario });
     }
 
     remove(usuario) {
         axios.delete(`${baseUrl}/${usuario.id}`)
             .then(response => {
-                const list = this.getUpdatedList(usuario, false)
-                this.setState({ list })
-                NotificationManager.success('Usuário excluído com sucesso', 'Excluir Usuário')
-            })
+                const list = this.getUpdatedList(usuario, false);
+                this.setState({ list });
+                NotificationManager.success('Usuário excluído com sucesso', 'Excluir Usuário');
+            });
     }
 
     render() {
